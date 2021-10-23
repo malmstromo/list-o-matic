5a3205706a2b848313f6aa6443231c485ca81966
Oskar Malmström <oskar.malmstrom@gmail.com>
2021-10-23 14:06:18 +0300

    restructure Login component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
 import { Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
@@ -9,13 +9,20 @@ interface LoginButtonProps {
 }
 
 export const LoginButton = ({ onClick, signedIn }: LoginButtonProps) => {
+  const SignedInMessage = () => {
+    return (
+      <Row>
+        <Col>
+          <span>Signed in!</span>
+          <FontAwesomeIcon title={'Signed in!'} icon={faCheckCircle} />
+        </Col>
+      </Row>
+    );
+  };
   return (
     <Fragment>
       {signedIn ? (
-        <Fragment>
-          <span>Signed in!</span>
-          <FontAwesomeIcon title={'Signed in!'} icon={faCheckCircle} />
-        </Fragment>
+        <SignedInMessage />
       ) : (
         <Button variant="primary" id="signIn" onClick={onClick}>
           Sign in
